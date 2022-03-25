2e9f0e298f5547940c4e527aa5bcf7beaae42ba4
gon2gon2 <ark5354@gmail.com>
2022-03-25 14:34:49 +0900

    add postgres config

---
+++ bowling-app/src/configs/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  // env에서 읽도록 변경 필요
+  type: 'postgres',
+  // host: 'localhost',
+  host: 'postgres',
+  port: 5432,
+  username: 'postgres',
+  password: 1234,
+  database: 'test',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
