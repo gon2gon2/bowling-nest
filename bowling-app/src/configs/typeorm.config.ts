f299e0a3fc0c040a06acb870979ae42918678ba9
gon2gon2 <ark5354@gmail.com>
2022-03-25 19:10:46 +0900

    hide pw

--- bowling-app/src/configs/typeorm.config.ts
+++ bowling-app/src/configs/typeorm.config.ts
@@ -3,12 +3,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeORMConfig: TypeOrmModuleOptions = {
   // env에서 읽도록 변경 필요
   type: 'postgres',
-  // host: 'localhost',
-  host: 'postgres',
+  host: process.env.DB_CONTAINER_NAME,
   port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'test',
+  username: process.env.DB_USER_ID,
+  password: process.env.DB_USER_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
